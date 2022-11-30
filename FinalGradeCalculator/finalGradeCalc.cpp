a37335600030ed7e10aaa3784076bc403e9aeb0a
AlinaSkowronek <a.skowronek.co@gmail.com>
2022-11-30 09:02:54 -0700

    Final Grade Calculator Code

---
+++ FinalGradeCalculator/finalGradeCalc.cpp
@@ -0,0 +1,17 @@
+/* Final Grade Calculator
+ * Alina Skowronek
+ * 30 November 2022
+ * Purpose:
+ *      Create a program to show the user the grade needed on their final to 
+ *      achieve the desired grade
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    cout << "Welcome to the Final Grade Calulator" << endl;
+    
+}
+
