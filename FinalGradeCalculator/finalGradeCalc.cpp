1d3ebeadd39c65421cf2f9a5d963404967f0efc2
AlinaSkowronek <a.skowronek.co@gmail.com>
2022-11-30 10:41:55 -0700

    Equation for Final Grade Calculation

--- FinalGradeCalculator/finalGradeCalc.cpp
+++ FinalGradeCalculator/finalGradeCalc.cpp
@@ -15,6 +15,8 @@ int main () {
     double currGrade;   // The grade the user currently has in the class
     double desGrade;    // The desired grade
     double finalAmt;    // The precentage the final is worth
+    double finalGrade;  // The grade the user needs on the final to get desired grade
+    double decimalAmt;  //Coverting the precent of final weight into a decimal
     cout << "\n\nWelcome to the Final Grade Calulator" << endl;
     cout << "\n\nHere you can calculate the grade needed on your final to achieve" <<
         " the desried grade in your class!" << endl;
@@ -25,5 +27,9 @@ int main () {
     cin >> desGrade;
     cout << "\n\nEnter the percent the final is worth: " << endl;
     cin >> finalAmt;
+
+    finalGrade = ((desGrade - (1 - (finalAmt / 100)) * currGrade) / finalAmt) * 100;
+
+    cout << finalGrade;
 }
 
