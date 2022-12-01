3322b3bafa14b201df7e5fc4dbdb468c95ac71d3
AlinaSkowronek <a.skowronek.co@gmail.com>
2022-12-01 16:51:10 -0700

    December Calender

---
+++ FinalGradeCalculator/Calender/calender.cpp
@@ -0,0 +1,25 @@
+/* Final Grade Calculator
+ * Alina Skowronek
+ * 1 December 2022
+ * Purpose:
+ *      Create a December calender to organize assignments, study times, and finals dates 
+ *      for finals season
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+
+using namespace std;
+
+int main () {
+    cout << "\n\n          December" << endl;
+    cout << "----------------------------" << endl;
+    cout << " S   M   T   W   Th   F   S" << endl;
+    cout << "                 1    2   3" << endl;
+    cout << " 4   5   6   7   8    9   10" << endl;
+    cout << " 11  12  13  14  15   16  17" << endl; 
+    cout << " 18  19  20  21  22   23  24" << endl;
+    cout << " 25  26  27  28  29   30  31" << endl;
+}
